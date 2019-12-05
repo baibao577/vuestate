78bc644e8b8372532e4bb9c1bb8f093f7673cc49
baibao <knotz22@gmail.com>
2019-12-05 20:44:02 +0700

    Test Vue State

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <AddTodo />
+      <Todos />
+    </div>
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos.vue'
+import AddTodo from './components/AddTodo.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Todos,
+    AddTodo
+  }
+}
+</script>
+
+<style>
+.container {
+  max-width: 500px;
+  margin: auto;
+}
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h3>Todos</h3>
+        <div class="todos">
+            <div v-for="todo in allTodos" :key="todo.id" class="todo">
+                {{ todo.title }}
+                <i @click="deleteTodo(todo.id)"><b>delete</b></i>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+    name: "Todos",
+    methods: {
+        ...mapActions(['fetchTodos', "deleteTodo"])
+    },
+    computed: mapGetters(['allTodos']),
+    created() {
+        this.fetchTodos();
+    }
+}
+</script>
+
+<style scoped>
+.todo {
+    background: salmon;
+    margin: 20px;
+    padding: 20px;
+}
+</style>
